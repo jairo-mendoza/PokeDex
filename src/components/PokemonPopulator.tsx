f009e43f4784737b7f9a34b5127026a84818ebcd
jmendoza271 <jmendoza271@toromail.csudh.edu>
2022-07-03 00:58:54 -0700

    Fix node modules and added responsive table view for Pokemon

--- src/components/PokemonPopulator.tsx
+++ src/components/PokemonPopulator.tsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import Alert from "react-bootstrap/Alert";
 import RetrievePokemon from "../services/PokemonRetriever";
 
 function PokemonPopulator() {
@@ -6,9 +8,26 @@ function PokemonPopulator() {
   console.log(pokeData);
   return (
     <div>
-      {pokeData.map((currentPokemon, index) => {
-        return <p key={index}>{currentPokemon["name"]}</p>;
-      })}
+      <Row>
+        {pokeData.map((currentPokemon, index) => {
+          return (
+            <Col
+              key={currentPokemon["id"]}
+              xs={12}
+              sm={12}
+              md={4}
+              lg={4}
+              xl={2}
+            >
+              <img
+                src={currentPokemon["sprites"]["front_default"]}
+                alt={currentPokemon["name"]}
+              />
+              <p>{currentPokemon["name"]}</p>
+            </Col>
+          );
+        })}
+      </Row>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+// Allowing bootstrap to work for all files
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
