3f472aed0e854589574ac7345c6f19671dbb4c86
jmendoza271 <jmendoza271@toromail.csudh.edu>
2022-07-13 23:14:49 -0700

    Fix StyledComponent warnings
    StyledComponents was giving back a warning, this was due to creating the StyledComponents inside the React component. Fix was to move it out of the React component.

--- src/components/poketable/cells/index.tsx
+++ src/components/poketable/cells/index.tsx
@@ -4,33 +4,33 @@ import styled from "styled-components";
 
 import { color } from "../../../colors/pokeTypes";
 
+// Styling
+const PokeCard = styled(Card)`
+  margin-bottom: 1em;
+  background-color: ${(props) => props.backgroundColor || "white"};
+  cursor: pointer;
+`;
+
+const Sprite = styled.img`
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 50%;
+`;
+
+const Text = styled.p`
+  color: white;
+  text-align: center;
+`;
+
 const PokeCell = (props: any) => {
-  // Styling
-  const PokeCard = styled(Card)`
-    margin-bottom: 1em;
-    background-color: ${color[props.type]};
-    cursor: pointer;
-  `;
-
-  const Sprite = styled.img`
-    display: block;
-    margin-left: auto;
-    margin-right: auto;
-    width: 50%;
-  `;
-
-  const Text = styled.p`
-    color: white;
-    text-align: center;
-  `;
-
   const openPokeData = () => {
     console.log(`Opening data about ${props.name}...`);
   };
 
   return (
     <Col key={props.id} xs={6} sm={4} md={4} lg={3} xl={2} xxl={2}>
-      <PokeCard onClick={openPokeData}>
+      <PokeCard backgroundColor={color[props.type]} onClick={openPokeData}>
         <Sprite src={props.sprite} alt={props.name} />
         <Text>{props.name}</Text>
         <Text>{props.type}</Text>
