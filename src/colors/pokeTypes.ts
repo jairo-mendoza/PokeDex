0a479802e14c6d232cad2780fc0ae3d5b994edcc
jmendoza271 <jmendoza271@toromail.csudh.edu>
2022-07-22 00:12:32 -0700

    Added pokemon type colors that were missing

--- src/colors/pokeTypes.ts
+++ src/colors/pokeTypes.ts
@@ -6,11 +6,13 @@
 
 export const color: any = {
   bug: "#A8B820",
+  dark: "#705848",
   dragon: "#7038F8",
   electric: "#F8D030",
   fairy: "#EE99AC",
   fighting: "#C03028",
   fire: "#F08030",
+  flying: "#A890F0",
   ghost: "#705898",
   grass: "#78C850",
   ground: "#E0C068",
@@ -19,5 +21,6 @@ export const color: any = {
   poison: "#A040A0",
   psychic: "#F85888",
   rock: "#B8A038",
+  steel: "#B8B8D0",
   water: "#6890F0",
 };
