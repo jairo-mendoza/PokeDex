314e1606d6de5aa3abd6ec593b27c10e29486889
jmendoza271 <jmendoza271@toromail.csudh.edu>
2022-08-01 19:09:30 -0700

    Add explicit typing for data recieved from PokeAPI

--- src/components/poketable/index.tsx
+++ src/components/poketable/index.tsx
@@ -1,36 +1,40 @@
+import { createContext } from "react";
+import styled from "styled-components";
+
 import { Row } from "react-bootstrap";
 
 import RetrievePokemon from "../../services/PokemonRetriever";
 import TableControlBar from "./controls";
 import PokeCell from "./cells";
-import styled from "styled-components";
 
 // Styling
 const StyledPokeTable = styled("div")`
-  margin-left: 10px;
-  margin-right: 10px;
+    margin-left: 10px;
+    margin-right: 10px;
 `;
 
+//const context = createContext();
+
 function PokeTable() {
-  const pokeData = RetrievePokemon();
-  return (
-    <StyledPokeTable>
-      <TableControlBar />
-      <Row>
-        {pokeData.map((currentPoke, index) => {
-          return (
-            <PokeCell
-              key={currentPoke["id"]}
-              id={currentPoke["id"]}
-              sprite={currentPoke["sprites"]["front_default"]}
-              name={currentPoke["name"]}
-              type={currentPoke["types"][0]["type"]["name"]}
-            />
-          );
-        })}
-      </Row>
-    </StyledPokeTable>
-  );
+    const pokeData = RetrievePokemon();
+    return (
+        <StyledPokeTable>
+            <TableControlBar />
+            <Row>
+                {pokeData.map((currentPoke) => {
+                    return (
+                        <PokeCell
+                            key={currentPoke.id}
+                            id={currentPoke.id}
+                            sprite={currentPoke.sprites.front_default}
+                            name={currentPoke.name}
+                            type={currentPoke.types[0].type.name}
+                        />
+                    );
+                })}
+            </Row>
+        </StyledPokeTable>
+    );
 }
 
 export default PokeTable;
---
+++ src/services/Pokemon.ts
@@ -0,0 +1,264 @@
+export interface Pokemon {
+    abilities: Abilities[];
+    base_experience: number;
+    forms: Forms[];
+    game_indices: GameIndices[];
+    height: number;
+    held_items: HeldItems[];
+    id: number;
+    is_default: boolean;
+    location_area_encounters: string;
+    moves: Moves[];
+    name: string;
+    order: number;
+    past_types: PastTypes[];
+    species: Species;
+    sprites: Sprites;
+    stats: Stats[];
+    types: Types[];
+    weight: number;
+}
+
+interface Abilities {
+    ability: {
+        name: string;
+        url: string;
+    };
+    is_hidden: boolean;
+    slot: number;
+}
+
+interface Forms {
+    name: string;
+    url: string;
+}
+
+interface GameIndices {
+    game_index: number;
+    version: {
+        name: string;
+        url: string;
+    };
+}
+
+interface HeldItems {
+    item: {
+        name: string;
+        url: string;
+    };
+    version_details: VersionDetails[];
+}
+
+interface Moves {
+    move: {
+        name: string;
+        url: string;
+    };
+    version_group_details: VersionGroupDetails[];
+}
+
+interface PastTypes {
+    generation: {
+        name: string;
+        url: string;
+    };
+    types: Types[];
+}
+
+interface Species {
+    name: string;
+    url: string;
+}
+
+interface Sprites {
+    back_default: string;
+    back_female: string;
+    back_shiny: string;
+    back_shiny_female: string;
+    front_default: string;
+    front_female: string;
+    front_shiny: string;
+    front_shiny_female: string;
+    other: {
+        dream_world: {
+            front_default: string;
+            front_female: string;
+        };
+        home: {
+            front_default: string;
+            front_female: string;
+            front_shiny: string;
+            front_shiny_female: string;
+        };
+        official_artwork: {
+            front_default: string;
+        };
+    };
+    versions: {
+        generation_i: {
+            red_blue: {
+                back_default: string;
+                back_gray: string;
+                back_transparent: string;
+                front_default: string;
+                front_gray: string;
+                front_transparent: string;
+            };
+            yellow: {
+                back_default: string;
+                back_gray: string;
+                back_transparent: string;
+                front_default: string;
+                front_gray: string;
+                front_transparent: string;
+            };
+        };
+        generation_ii: {
+            crystal: {
+                back_default: string;
+                back_shiny: string;
+                back_shiny_transparent: string;
+                back_transparent: string;
+                front_default: string;
+                front_shiny: string;
+                front_shiny_transparent: string;
+                front_transparent: string;
+            };
+            gold: {
+                back_default: string;
+                back_shiny: string;
+                front_default: string;
+                front_shiny: string;
+                front_transparent: string;
+            };
+            silver: {
+                back_default: string;
+                back_shiny: string;
+                front_default: string;
+                front_shiny: string;
+                front_transparent: string;
+            };
+        };
+        generation_iii: {
+            emerald: {
+                front_default: string;
+                front_shiny: string;
+            };
+            firered_leafgreen: {
+                back_default: string;
+                back_shiny: string;
+                front_default: string;
+                front_shiny: string;
+            };
+            ruby_sapphire: {
+                back_default: string;
+                back_shiny: string;
+                front_default: string;
+                front_shiny: string;
+            };
+        };
+        generation_iv: {
+            diamond_pearl: {
+                back_default: string;
+                back_female: string;
+                back_shiny: string;
+                back_shiny_female: string;
+                front_default: string;
+                front_female: string;
+                front_shiny: string;
+                front_shiny_female: string;
+            };
+        };
+        generation_v: {
+            black_white: {
+                animated: {
+                    back_default: string;
+                    back_female: string;
+                    back_shiny: string;
+                    back_shiny_female: string;
+                    front_default: string;
+                    front_female: string;
+                    front_shiny: string;
+                    front_shiny_female: string;
+                };
+                back_default: string;
+                back_female: string;
+                back_shiny: string;
+                back_shiny_female: string;
+                front_default: string;
+                front_female: string;
+                front_shiny: string;
+                front_shiny_female: string;
+            };
+        };
+        generation_vi: {
+            omegaruby_alphasapphire: {
+                front_default: string;
+                front_female: string;
+                front_shiny: string;
+                front_shiny_female: string;
+            };
+            x_y: {
+                front_default: string;
+                front_female: string;
+                front_shiny: string;
+                front_shiny_female: string;
+            };
+        };
+        generation_vii: {
+            icons: {
+                front_default: string;
+                front_female: string;
+            };
+            ultra_sun_ultra_moon: {
+                front_default: string;
+                front_female: string;
+                front_shiny: string;
+                front_shiny_female: string;
+            };
+        };
+        generation_viii: {
+            icons: {
+                front_default: string;
+                front_female: string;
+            };
+        };
+    };
+}
+
+interface Stats {
+    base_stat: number;
+    effort: number;
+    stat: {
+        name: string;
+        url: string;
+    };
+}
+
+interface Types {
+    slot: number;
+    type: {
+        name: string;
+        url: string;
+    };
+}
+
+interface VersionDetails {
+    rarity: number;
+    version: {
+        name: string;
+        url: string;
+    };
+}
+
+interface VersionGroupDetails {
+    level_learned_at: number;
+    move_learn_method: {
+        name: string;
+        url: string;
+    };
+    version_group: {
+        name: string;
+        url: string;
+    };
+}
