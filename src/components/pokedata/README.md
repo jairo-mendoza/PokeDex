8e773659e9c4781d5764901978f36b5c2fcfa611
jairo-mendoza <jairomendoza629@gmail.com>
2023-01-16 22:33:22 -0800

    Initial start to PokeModal
    User can now click on a PokeCell that will bring up a modal for that specific Pokemon. As of this commit, only the pokemon's name is shown. Will continue to add more useful data.

---
+++ src/components/pokedata/README.md
@@ -0,0 +1,3 @@
+### Function
+
+This component appears whenever you click a pokemon cell located on the poketable.
--- src/components/poketable/cells/index.tsx
+++ src/components/poketable/cells/index.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Card } from "react-bootstrap";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
 import { color } from "../../../colors/pokeTypes";
+import PokeModal from "../../pokedata/modal";
 
 // Styling
 const PokeCard = styled(Card)`
@@ -26,18 +27,33 @@ const Text = styled.p`
 `;
 
 const PokeCell = (props: any) => {
+    const [isClicked, setIsClicked] = useState(false);
+
+    // NOTE: Seems like onClick property has to hold a function like this, otherwise it automatically runs code on all renders even
+    // without a user's click (will look more into this)
     const openPokeData = () => {
         console.log(`Opening data about ${props.name}...`);
+        setIsClicked(true);
     };
 
     return (
-        <Col key={props.id} xs={6} sm={4} md={4} lg={3} xl={2} xxl={2}>
-            <PokeCard background={color[props.type]} onClick={openPokeData}>
-                <Sprite src={props.sprite} alt={props.name} />
-                <Text>{props.name}</Text>
-                <Text>{props.type}</Text>
-            </PokeCard>
-        </Col>
+        <>
+            <Col key={props.id} xs={6} sm={4} md={4} lg={3} xl={2} xxl={2}>
+                <PokeCard background={color[props.type]} onClick={openPokeData}>
+                    <Sprite src={props.sprite} alt={props.name} />
+                    <Text>{props.name}</Text>
+                    <Text>{props.type}</Text>
+                </PokeCard>
+            </Col>
+
+            {isClicked && (
+                <PokeModal
+                    show={isClicked}
+                    name={props.name}
+                    onHide={() => setIsClicked(false)}
+                />
+            )}
+        </>
     );
 };
 
