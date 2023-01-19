83c4d4bd72e2e4fab5be837bcbabd788706f5c3d
jairo-mendoza <jairomendoza629@gmail.com>
2023-01-19 00:52:45 -0800

    Added animated sprites to PokeModal

--- src/components/pokedata/modal/index.tsx
+++ src/components/pokedata/modal/index.tsx
@@ -1,5 +1,17 @@
 import Modal from "react-bootstrap/Modal";
+import styled from "styled-components";
 import { Pokemon } from "../../../services/Pokemon";
+
+/*
+ * Sprites are from Gen V, black and white
+ *
+ */
+const Sprite = styled.img`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 30%;
+`;
 
 const PokeModal = (props: any) => {
     const curPokemon: Pokemon = props.pokemon;
@@ -13,15 +25,21 @@ const PokeModal = (props: any) => {
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
-                    {curPokemon.name}
+                    {curPokemon.name} #{curPokemon.id}
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo
-                    odio, dapibus ac facilisis in, egestas eget quam. Morbi leo
-                    risus, porta ac consectetur ac, vestibulum at eros.
-                </p>
+                <Sprite
+                    src={
+                        curPokemon.sprites.versions["generation-v"][
+                            "black-white"
+                        ].animated.front_default
+                    }
+                    alt={curPokemon.name}
+                />
+                {curPokemon.types.map((curType) => {
+                    return <p key={curType.slot}>{curType.type.name}</p>;
+                })}
             </Modal.Body>
         </Modal>
     );
