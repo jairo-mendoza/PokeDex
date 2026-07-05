582597cb496d36ee6abb823c993e390e5aa47167
jairo-mendoza <jairomendoza629@gmail.com>
2026-07-05 15:57:13 -0700

    style: add color tokens

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from './api/queryClient.ts'
+import './styles/tokens.css'
 import './index.css'
 import App from './App.tsx'
 
